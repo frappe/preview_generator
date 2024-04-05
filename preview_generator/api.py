cfeeb58c7e41928db03693aaf8e34d92208d2221
Suraj Shetty <surajshetty3416@gmail.com>
2024-04-05 12:52:13 +0530

    fix: unescape HTML before generating preview

--- preview_generator/api.py
+++ preview_generator/api.py
@@ -1,16 +1,19 @@
+import frappe
 from playwright.sync_api import sync_playwright
+from frappe.rate_limiter import rate_limit
+import html as html_parser
 
 @frappe.whitelist(allow_guest=True)
 @rate_limit(limit=60, seconds=60)
 def generate_preview(html):
-	with sync_playwright() as p:
-
-		browser = p.chromium.launch()
-		page = browser.new_page()
-		page.set_content(html)
-		page.wait_for_load_state('networkidle')
-		# page.screenshot(path=output_path, quality=30, type='jpeg')
-		image = page.screenshot(type='jpeg', quality=30)
-		frappe.local.response.filename = 'preview.jpg'
-		frappe.local.response.filecontent = image
-		browser.close()
+	playwright = sync_playwright().start()
+	browser = playwright.chromium.launch()
+	context = browser.new_context()
+	page = context.new_page()
+	page.set_content(html_parser.unescape(html))
+	page.wait_for_load_state('networkidle')
+	image = page.screenshot(type='jpeg', quality=30)
+	frappe.local.response.filename = 'preview.jpg'
+	frappe.local.response.filecontent = image
+	frappe.local.response.type = "download"
+	context.close()
