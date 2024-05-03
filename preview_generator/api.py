851ea3637d43349573eb2f99f7c3fe8188ab7212
Suraj Shetty <surajshetty3416@gmail.com>
2024-05-03 14:54:46 +0530

    fix: playwright._impl._errors.Error

--- preview_generator/api.py
+++ preview_generator/api.py
@@ -6,14 +6,14 @@ import html as html_parser
 @frappe.whitelist(allow_guest=True)
 @rate_limit(limit=60, seconds=60)
 def generate_preview(html):
-	playwright = sync_playwright().start()
-	browser = playwright.chromium.launch()
-	context = browser.new_context()
-	page = context.new_page()
-	page.set_content(html_parser.unescape(html))
-	page.wait_for_load_state('networkidle')
-	image = page.screenshot(type='jpeg', quality=30)
-	frappe.local.response.filename = 'preview.jpg'
-	frappe.local.response.filecontent = image
-	frappe.local.response.type = "download"
-	context.close()
+	with sync_playwright() as playwright:
+		browser = playwright.chromium.launch()
+		context = browser.new_context()
+		page = context.new_page()
+		page.set_content(html_parser.unescape(html))
+		page.wait_for_load_state('networkidle')
+		image = page.screenshot(type='jpeg', quality=30)
+		frappe.local.response.filename = 'preview.jpg'
+		frappe.local.response.filecontent = image
+		frappe.local.response.type = "download"
+		context.close()
