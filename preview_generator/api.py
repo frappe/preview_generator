24a952713aa46e30c13f24c1ff0ade69dc1c1a50
Saqib Ansari <nextchamp.saqib@gmail.com>
2024-10-30 12:04:30 +0530

    feat: generate preview from url

--- preview_generator/api.py
+++ preview_generator/api.py
@@ -17,3 +17,21 @@ def generate_preview(html):
 		frappe.local.response.filecontent = image
 		frappe.local.response.type = "download"
 		context.close()
+
+
+@frappe.whitelist(allow_guest=True)
+@rate_limit(limit=60, seconds=60)
+def generate_preview_from_url(url: str, wait_for: int = 0):
+	with sync_playwright() as playwright:
+		browser = playwright.chromium.launch()
+		context = browser.new_context()
+		page = context.new_page()
+		page.goto(url)
+		page.wait_for_load_state('networkidle')
+		if wait_for:
+			page.wait_for_timeout(wait_for)
+		image = page.screenshot(type='jpeg', quality=30)
+		frappe.local.response.filename = 'preview.jpg'
+		frappe.local.response.filecontent = image
+		frappe.local.response.type = "download"
+		context.close()
